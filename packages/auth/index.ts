95128794f47b4fd8d7d48a1e615618ad333d71c9
Eduardo Viero <duduviero@gmail.com>
2025-11-26 16:31:33 -0300

    Abilities

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -13,6 +13,7 @@ import { organizationMemberSubject } from './src/subjects/organization-member';
 import { inviteSubject } from './src/subjects/invite';
 import { categorySubject } from './src/subjects/category';
 import { accountSubject } from './src/subjects/account';
+import type { User } from './src/models/user';
 
 const appAbilitiesSchema = z.union([
   userSubject,
@@ -30,3 +31,24 @@ type AppAbilities = z.infer<typeof appAbilitiesSchema>;
 
 export type AppAbility = MongoAbility<AppAbilities>;
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>;
+
+export function defineAbilityFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility)
+
+  // if (typeof permissions[user.role] !== 'function') {
+  //     throw new Error(`Permissions for role ${user.role} not found.`);
+  //   }
+
+  //   permissions[user.role](user, builder);
+
+  const ability = builder.build({
+    detectSubjectType(subject) {
+      return subject.__typename;
+    },
+  });
+
+  ability.can = ability.can.bind(ability);
+  ability.cannot = ability.cannot.bind(ability);
+
+  return ability;
+}
