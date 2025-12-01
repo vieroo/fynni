c29a4bc5b4987fbc919b01aa414e2b7959bd2d66
Eduardo Viero <duduviero@gmail.com>
2025-12-01 17:32:25 -0300

    Prisma Schema

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,41 @@
-import { fastify } from "fastify";
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import ScalarApiReference from '@scalar/fastify-api-reference'
+import { fastify } from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
-	console.log("Server is running on http://localhost:3333");
-});
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  // credentials: true,
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Fynni API Documentation',
+      description: 'API documentation for Fynni application',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(ScalarApiReference, {
+  routePrefix: '/docs',
+})
+
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+  console.log('🔥 Server is running on http://localhost:3333')
+  console.log('📘 Docs available at http://localhost:3333/docs')
+})
